e22c5d5d9c19455107d135672bedab51ada73b05
venkatesho17 <venkatesho17@users.noreply.github.com>
2026-02-06 23:47:31 +0530

    chore: add cn utility to lib/utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,3 @@
-import { clsx, type ClassValue } from 'clsx'
-import { twMerge } from 'tailwind-merge'
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+export function cn(...inputs: Array<string | false | null | undefined>) {
+  return inputs.filter(Boolean).join(' ');
 }
